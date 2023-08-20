407caaac8ddd3613676d5f692e13c128b3b322e0
Harshal Abak <abakharshal@gmail.com>
2023-08-20 15:37:19 +0530

    auth remove

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDapzyVgu5Nv4i3s7TIw5fKFIeLS4YDB9g",
-  authDomain: "rapid-crew-1947c.firebaseapp.com",
-  projectId: "rapid-crew-1947c",
-  storageBucket: "rapid-crew-1947c.appspot.com",
-  messagingSenderId: "674640692953",
-  appId: "1:674640692953:web:c8e98832e1fa86a3ffa0c8",
-  measurementId: "G-QX9814DMG9",
+  apiKey: "",
+  authDomain: "",
+  projectId:"" ,
+  storageBucket:"" ,
+  messagingSenderId: "",
+  appId:"" ,
+  measurementId: ""
 };
 
 // Initialize Firebase
