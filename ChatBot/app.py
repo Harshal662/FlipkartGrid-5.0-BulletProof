197334424738a972d379472e931f03e8cc33f99c
Harshal Abak <abakharshal@gmail.com>
2023-08-20 12:52:54 +0530

    multiple file read

--- ChatBot/app.py
+++ ChatBot/app.py
@@ -13,30 +13,10 @@ from langchain.chains import ConversationalRetrievalChain
 app = Flask(__name__)
 
 # Set your OpenAI API key as an environment variable
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 
-# Load the PDF file
-pdf_path = "./new.pdf"
-try:
-    loader = PyPDFLoader(pdf_path)
-    pages = loader.load_and_split()
-
-    chunks = pages
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page_num in range(doc.page_count):
-        page = doc.load_page(page_num)
-        text += page.get_text()
-    doc.close()
-except Exception as e:
-    print(f"Error loading PDF: {e}")
-    text = ""
-
-# Initialize tokenizer, splitter, embeddings, and models
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-
-with open('attention_is_all_you_need.txt', 'w', encoding="utf-8") as f:
-    f.write(text)
+embeddings = OpenAIEmbeddings()
 
 # Define the text splitting function and embedding method (you may need to modify this)
 
@@ -45,17 +25,42 @@ def count_tokens(text: str) -> int:
     return len(tokenizer.encode(text))
 
 
+# Create the text splitter
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=512,
     chunk_overlap=24,
     length_function=count_tokens,
 )
 
-chunks = text_splitter.create_documents([text])
+# Load and process multiple PDF files
+pdf_folder = "./pdf_files"  # Update this path to the folder containing your PDF files
+pdf_files = [file for file in os.listdir(pdf_folder) if file.endswith(".pdf")]
 
-embeddings = OpenAIEmbeddings()
+chunks = []
+
+for pdf_file in pdf_files:
+    pdf_path = os.path.join(pdf_folder, pdf_file)
+    try:
+        loader = PyPDFLoader(pdf_path)
+        pages = loader.load_and_split()
+
+        text = ""
+        doc = fitz.open(pdf_path)
+        for page_num in range(doc.page_count):
+            page = doc.load_page(page_num)
+            text += page.get_text()
+        doc.close()
+
+        # Create documents using the text splitter
+        chunks.extend(text_splitter.create_documents([text]))
+
+    except Exception as e:
+        print(f"Error loading PDF {pdf_file}: {e}")
+
+# Create FAISS index from documents
 db = FAISS.from_documents(chunks, embeddings)
 
+# Load question answering chain
 chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
 
 # Create the conversational retrieval chain
@@ -63,8 +68,6 @@ qa = ConversationalRetrievalChain.from_llm(
     OpenAI(temperature=0.1), db.as_retriever())
 
 chat_history = []
-
-# Define routes and views
 
 
 @app.route('/')
